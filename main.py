736a2bc1510a67ee04dc82b75610b0f4f46272b5
Khalid-oti <otaibik2233@gmail.com>
2025-04-07 18:09:43 +0300

    imported sys, any text file can be passed in

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from stats import get_word_count
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1] #"books/frankenstein.txt"
     book_contents = get_book_contents(book_path)
     word_count = get_word_count(book_contents)
     character_count = get_character_count(book_contents)
     letter_count = get_letter_count(character_count)
     print(f"{word_count} words found in the document")
     #print(character_count)
-    print(letter_count)
+    #print(letter_count)
 
 def get_book_contents(path):
     with open(path) as f:
